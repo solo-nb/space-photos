1a350b06a298519fa5b5d53ccd6142be0353d64d
solo-nb <to.solo.nb@gmail.com>
2023-08-14 12:06:48 +0500

    Added argument id of launch to fetch_spacex_images

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,26 +1,37 @@
 import requests
 from service_func import download_images
 import os
+import argparse
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_urls_from_spacex() -> list:
-    url = 'https://api.spacexdata.com/v5/launches/'
+def get_urls_from_spacex(id: str) -> list:
+    url = 'https://api.spacexdata.com/v5/launches/{}'.format(id)
 
     response = requests.get(url)
     response.raise_for_status()
 
+    if id:
+        return list(response.json()['links']['flickr']['original'])
+
     for launch in response.json():
         if len(launch['links']['flickr']['original']) > 0:
-            spacex_urls = list(launch['links']['flickr']['original'])
-            break
-
-    return spacex_urls
+            return list(launch['links']['flickr']['original'])
 
 
 def main():
-    espacex_urls = get_urls_from_spacex()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "id",
+        nargs='?',
+        default='',
+        help="download specific launch id"
+    )
+    args = parser.parse_args()
+    id = args.id
+
+    espacex_urls = get_urls_from_spacex(id)
     download_images(espacex_urls, os.getenv('CATALOG'))
 
 
