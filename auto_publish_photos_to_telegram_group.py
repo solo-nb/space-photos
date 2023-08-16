1f2c4392e50d336bb33e5ca1def8599252e32144
solo-nb <to.solo.nb@gmail.com>
2023-08-16 13:31:31 +0500

    Fixed auto_publish script

--- auto_publish_photos_to_telegram_group.py
+++ auto_publish_photos_to_telegram_group.py
@@ -1,22 +1,13 @@
-from os import getenv, walk, path
+from os import getenv, path
 from random import shuffle
-from service_func import publish_photo_to_telegram, get_extension
+from service_func import publish_photo_to_telegram, get_extension, get_images
 from time import sleep
 
 
-def get_images(catalog: str) -> list:
-    images = []
-    for adr, dir, file in walk(catalog):
-        for img in file:
-            images.append(path.join(adr, img))
-    return images
-
-
-def main():
-    token = getenv('TELEGRAM_TOKEN')
-    id_group = getenv('TELEGRAM_GROUP_ID')
-    catalog = getenv('CATALOG')
-    pause_time = int(getenv('PAUSE_TIME'))
+def auto_publish_photos_to_telegram(token: str,
+                                    id_group: str,
+                                    catalog: str,
+                                    pause_time: int):
     images = get_images(catalog)
 
     while True:
@@ -37,6 +28,15 @@ def main():
                 index = 0
                 shuffle(images)
             sleep(pause_time * 60 * 60)
+
+
+def main():
+    token = getenv('TELEGRAM_TOKEN')
+    id_group = getenv('TELEGRAM_GROUP_ID')
+    catalog = getenv('CATALOG')
+    pause_time = int(getenv('PAUSE_TIME'))
+
+    auto_publish_photos_to_telegram(token, id_group, catalog, pause_time)
 
 
 if __name__ == '__main__':
